1c2c80ec83dc4a9e5427f6b3f1129a57725ccbd5
Titus <166182384+NdabaTitus@users.noreply.github.com>
2026-05-20 00:06:57 +0200

    Add Emergency Screen

--- app/src/main/java/EmergencyActivity.kt
+++ app/src/main/java/EmergencyActivity.kt
@@ -3,7 +3,7 @@ package com.example.campusnavigator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class EmergencyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_emergency)
